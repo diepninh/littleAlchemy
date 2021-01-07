837125eeaea2bbcb200e44cc353de2dfc376e370
Hong Diep <diep@pop-os.localdomain>
2021-01-07 10:21:33 +0700

    import all image

---
+++ src/image/ImageTypes.js
@@ -0,0 +1,13 @@
+
+import air from '../image/air.png';
+import earth from  '../image/earth.png';
+import water from '../image/drop.png';
+import fire from '../image/fire.png' ;
+import steam from '../image/steam.png';
+import dush from '../image/dush.png';
+import energy from '../image/energy.png';
+import lava from '../image/lava.png';
+import osiban from '../image/osiban.png';
+import psi from '../image/psi.png';
+
+export   {air , earth , water , fire,steam , dush , energy , lava, osiban, psi};
