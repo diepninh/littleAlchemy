b750e9545f46d6bf5d0c571bd1075c150752c729
Hong Diep <diep@pop-os.localdomain>
2021-01-07 16:53:48 +0700

    use array

--- src/components/Replice.jsx
+++ src/components/Replice.jsx
@@ -1,13 +1,13 @@
-
+import 'lodash';
 export const Replice = (image, imageIngre, top, left) => {
-  const waterObj = { top , left ,  topStatic: 100, leftStatic: 20, image: 'water'};
-  const waterObjStatic = { top: 100 , left: 20 ,  topStatic: 100, leftStatic: 20, image: 'water'};
-  const airObj = { top , left ,  topStatic: 10, leftStatic: 20, image: 'air'};
-  const airObjStatic = { top: 10 , left: 20 ,  topStatic: 10, leftStatic: 20, image: 'air'};
-  const fireObj = { top , left ,  topStatic: 100, leftStatic: 100, image: 'fire'};
-  const fireObjStatic = { top: 100 , left: 100 ,  topStatic: 100, leftStatic: 100, image: 'fire'};
-  const earthObj = { top , left ,  topStatic: 100, leftStatic: 20, image: 'earth'};
-  const earthObjStatic = { top: 100 , left: 100 ,  topStatic: 100, leftStatic: 100, image: 'earth'};
+  const waterObj = { top, left, topStatic: 100, leftStatic: 20, image: 'water' };
+  const waterObjStatic = { top: 100, left: 20, topStatic: 100, leftStatic: 20, image: 'water' };
+  const airObj = { top, left, topStatic: 10, leftStatic: 20, image: 'air' };
+  const airObjStatic = { top: 10, left: 20, topStatic: 10, leftStatic: 20, image: 'air' };
+  const fireObj = { top, left, topStatic: 100, leftStatic: 100, image: 'fire' };
+  const fireObjStatic = { top: 100, left: 100, topStatic: 100, leftStatic: 100, image: 'fire' };
+  const earthObj = { top, left, topStatic: 100, leftStatic: 20, image: 'earth' };
+  const earthObjStatic = { top: 100, left: 100, topStatic: 100, leftStatic: 100, image: 'earth' };
   const smokeObj = { top: top, left: left, topStatic: 200, leftStatic: 10, image: 'steam' };
   const smokeObjStatic = { top: 200, left: 10, topStatic: 200, leftStatic: 10, image: 'steam' };
   const dushObj = { top, left, topStatic: 200, leftStatic: 100, image: 'dush' };
@@ -19,40 +19,37 @@ export const Replice = (image, imageIngre, top, left) => {
   const psiObj = { top, left, topStatic: 400, leftStatic: 100, image: 'psi' };
   const psiObjStatic = { top: 400, left: 100, topStatic: 400, leftStatic: 100, image: 'psi' };
 
-  switch(image){
-    case 'water':
-      switch(imageIngre){
-        case 'fire':
-          return { ObjStatic : smokeObjStatic, Obj : smokeObj , imageCreate: 'steam'};
-        default :
-          return { ObjStatic : waterObjStatic, Obj : waterObj , imageCreate: 'water'};
-      }
-    case 'air':
-      switch(imageIngre){
-        case 'fire':
-          return { ObjStatic : energyObjStatic, Obj : energyObj , imageCreate: 'energy'};
-        case 'earth':
-          return { ObjStatic : dushObjStatic, Obj : dushObj , imageCreate: 'dush'};
-        default :
-          return { ObjStatic : airObjStatic, Obj : airObj , imageCreate: 'air'};
-      }
-    case 'fire':
-      switch(imageIngre){
-        case 'fire':
-          return { ObjStatic : smokeObjStatic, Obj : smokeObj , imageCreate: 'steam'};
-        case 'earth':
-          return { ObjStatic : lavaObjStatic, Obj : lavaObj , imageCreate: 'lava'};
-        default :
-          return { ObjStatic : fireObjStatic, Obj : fireObj , imageCreate: 'fire'};
-      }
-    case 'earth':
-      switch(imageIngre){
-        case 'earth':
-          return { ObjStatic : psiObjStatic, Obj : psiObj , imageCreate: 'psi'};
-        default :
-          return { ObjStatic : earthObjStatic, Obj : earthObj , imageCreate: 'earth'};
-      }
-    default:
-      return { ObjStatic : '', Obj :  '' , imageCreate: ''};
+  const recipe = [
+    'water',
+    'air',
+    'fire',
+    'earth',
+    'waterfire',
+    'firewater',
+    'airfire',
+    'airearth',
+    'fireearth',
+    'earthearth',
+    'earthfire']
+  const chemical = [
+    { ObjStatic: waterObjStatic, Obj: waterObj, imageCreate: 'water' },
+    { ObjStatic: airObjStatic, Obj: airObj, imageCreate: 'air' },
+    { ObjStatic: fireObjStatic, Obj: fireObj, imageCreate: 'fire' },
+    { ObjStatic: earthObjStatic, Obj: earthObj, imageCreate: 'earth' },
+    { ObjStatic: smokeObjStatic, Obj: smokeObj, imageCreate: 'steam' },
+    { ObjStatic: smokeObjStatic, Obj: smokeObj, imageCreate: 'steam' },
+    { ObjStatic: energyObjStatic, Obj: energyObj, imageCreate: 'energy' },
+    { ObjStatic: dushObjStatic, Obj: dushObj, imageCreate: 'dush' },
+    { ObjStatic : lavaObjStatic, Obj : lavaObj , imageCreate: 'lava'},
+    { ObjStatic : psiObjStatic, Obj : psiObj , imageCreate: 'psi'},
+    { ObjStatic : lavaObjStatic, Obj : lavaObj , imageCreate: 'lava'}
+  ];
+
+  for (let i = 0; i < recipe.length; i++) {
+
+    if (image + imageIngre === recipe[i]) {
+      return chemical[i];
+    }
   }
+  return _.find(chemical, { imageCreate: image });
 }
