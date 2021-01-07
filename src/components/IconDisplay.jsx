c5ae2f7cf28ddaca740eafb06c0cac9f027b5c91
Hong Diep <diep@pop-os.localdomain>
2021-01-07 10:19:06 +0700

    change import

--- src/components/IconDisplay.jsx
+++ src/components/IconDisplay.jsx
@@ -1,35 +1,26 @@
 import React from 'react';
-import air from '../image/air.png';
-import earth from  '../image/earth.png';
-import water from '../image/drop.png';
-import fire from '../image/fire.png' ;
-import steam from '../image/steam.png';
-import dush from '../image/dush.png';
-import energy from '../image/energy.png';
-import lava from '../image/lava.png';
-import osiban from '../image/osiban.png';
-import psi from '../image/psi.png';
+import * as TYPE from '../image/ImageTypes.js';
 
 export  const IconDisplay = ({image}) =>{
   switch( image ){
     case 'air':
-      return <img src={air} width={60} height={60}/>
+      return <img src={TYPE.air} width={60} height={60}/>
     case 'earth':
-      return <img src={earth} width={60} height={60}/>
+      return <img src={TYPE.earth} width={60} height={60}/>
     case 'water':
-      return <img src={water} width={60} height={60}/>
+      return <img src={TYPE.water} width={60} height={60}/>
     case 'fire':
-      return <img src={fire} width={60} height={60}/>
+      return <img src={TYPE.fire} width={60} height={60}/>
     case 'steam':
-      return <img src={steam} width={60} height={60}/>
+      return <img src={TYPE.steam} width={60} height={60}/>
     case 'dush':
-      return <img src={dush} width={60}  height={60}/>
+      return <img src={TYPE.dush} width={60}  height={60}/>
     case 'energy':
-      return <img src={energy} width={60}  height={60}/>
+      return <img src={TYPE.energy} width={60}  height={60}/>
     case 'lava':
-      return <img src={lava} width={60}  height ={60}/>
+      return <img src={TYPE.lava} width={60}  height ={60}/>
     case 'osiban':
-      return <img src={osiban} width={60}  height={60}/>
+      return <img src={TYPE.osiban} width={60}  height={60}/>
     case 'psi':
       return <img src={psi} width={60}  height={60}/>
     default:
